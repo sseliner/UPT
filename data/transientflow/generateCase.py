22bc241f7834ab9f99706477c5d4848b8b7ebc23
sseliner <stephan.seliner@ost.ch>
2025-03-19 09:07:46 +0100

    Refactoring & Added PreProcessing
    - Save .th files of x, y, msh with precison fp16 instead of fp32
    - Removed creation of GIF & not needed methods

--- data/transientflow/generateCase.py
+++ data/transientflow/generateCase.py
@@ -29,7 +29,8 @@ from MeshGenerator import generate_mesh, sort_points
 def generate_object_mask(sol_dir, x_res, y_res):
     msh = meshio.read(sol_dir + "/mesh.msh")
 
-    meshio.write(sol_dir + "/mesh_legacy.msh", msh, file_format="gmsh22", binary=False)
+    # Needed for the gmshToFoam-Solver, which needs the gmsh22 as format
+    meshio.write(sol_dir + "/mesh_solver_format.msh", msh, file_format="gmsh22", binary=False)
 
     eps = 0.0075
     min_samples = 2
@@ -115,13 +116,13 @@ def scatter_plot(arg):
     return Image.open(img_buf)
 
 
-def prepareCase(src, dest, n_points, velocity, x_res=384, y_res=256):
+def prepareCase(src, dest, n_objects, velocity, x_res=384, y_res=256):
     if os.path.exists(dest) and os.path.isdir(dest):
         shutil.rmtree(dest)
     shutil.copytree(src, dest)
 
     while True:
-        process = Process(target=generate_mesh, args=(dest + "mesh.msh", n_points))
+        process = Process(target=generate_mesh, args=(dest + "mesh.msh", n_objects))
         try:
             process.start()
             process.join()
@@ -132,15 +133,16 @@ def prepareCase(src, dest, n_points, velocity, x_res=384, y_res=256):
 
         object_mask, n_detected_objects = generate_object_mask(dest, x_res, y_res)
 
-        if process.exitcode == 0 and object_mask is not None and n_points == n_detected_objects:
+        if process.exitcode == 0 and object_mask is not None and n_objects == n_detected_objects:
             print("Mesh generated")
             break
         else:
             print("retry mesh generation")
-            print("objects (expected vs detected):", n_points, n_detected_objects)
+            print("objects (expected vs detected):", n_objects, n_detected_objects)
             time.sleep(3)
 
-    runner = BasicRunner(argv=["gmshToFoam", "-case", dest, dest + "mesh_legacy.msh"], logname="logifle", noLog=True)
+    runner = BasicRunner(argv=["gmshToFoam", "-case", dest, dest + "mesh_solver_format.msh"], logname="logifle",
+                         noLog=True, silent=True)
     runner.start()
 
     f = ParsedBoundaryDict(dest + "constant/polyMesh/boundary")
@@ -154,29 +156,17 @@ def prepareCase(src, dest, n_points, velocity, x_res=384, y_res=256):
     return object_mask
 
 
-def get_current_case(parent_directory):
-    directories = [d for d in os.listdir(parent_directory)
-                   if os.path.isdir(os.path.join(parent_directory, d))]
-    if not directories:
-        return 0
-    existing_numbers = [int(d.split('_')[1]) for d in directories
-                        if d.startswith("case_") and d[5:].isdigit()]
-    for i in range(1, max(existing_numbers) + 2):
-        if i not in existing_numbers:
-            return i
+def get_cases_count(parent_directory):
+    return len([d for d in os.listdir(parent_directory) if os.path.isdir(os.path.join(parent_directory, d))])
 
 
-def process_case(args, current_case_number, work_dir, target_dir, save_raw=1):
-    n_points = random.randint(1, args.n_objects)
+def process_case(args, current_case_number, work_dir, dest_dir, save_raw=1):
+    n_objects = random.randint(1, args.n_objects)
     velocity = random.uniform(0.01, 0.06)
     nr_time_steps = 0
 
     crash_counter = 0
-    object_mask = prepareCase(args.empty_case, work_dir, n_points, velocity)
-
-    msh = meshio.read(work_dir + "/mesh.msh")
-    triangles = msh.cells_dict['triangle'][(msh.points[msh.cells_dict['triangle']][:, :, -1] == 0)[:, 0]]
-    mesh_points = msh.points
+    object_mask = prepareCase(args.empty_case, work_dir, n_objects, velocity)
 
     time.sleep(5)
 
@@ -204,7 +194,7 @@ def process_case(args, current_case_number, work_dir, target_dir, save_raw=1):
     except IndexError:
         print("List out of bound, restarting outer loop")
 
-    solution_dir = target_dir + "/case_" + str(current_case_number) + "/"
+    solution_dir = dest_dir + "/case_" + str(current_case_number) + "/"
     try:
         os.mkdir(solution_dir)
     except OSError as error:
@@ -223,7 +213,9 @@ def process_case(args, current_case_number, work_dir, target_dir, save_raw=1):
     with ParsedParameterFile(work_dir + "0/U") as f:
         initial_velocity = float(str(f['internalField']).split('(')[1].split(" ")[0])
 
-    simulation_description = {"initial_velocity": initial_velocity, "n_objects": n_points}
+    simulation_description = {"initial_velocity": initial_velocity, "n_objects": n_objects}
+    torch.save(torch.tensor(initial_velocity), os.path.join(solution_dir, f"U_init.th"))
+    torch.save(torch.tensor(n_objects), os.path.join(solution_dir, f"num_objects.th"))
 
     with open(solution_dir + "simulation_description.pkl", 'wb') as handle:
         pickle.dump(simulation_description, handle)
@@ -238,17 +230,14 @@ def process_case(args, current_case_number, work_dir, target_dir, save_raw=1):
     U = [readU((i, work_dir)) for i in range(1, max_time_steps + 1)]
     p = [readp((i, work_dir)) for i in range(1, max_time_steps + 1)]
 
-    U_stacked = torch.stack(U)
-
-    x = torch.tensor(x)
-    y = torch.tensor(y)
-    v = torch.sqrt(U_stacked[:, 0, :] ** 2 + U_stacked[:, 1, :] ** 2 + U_stacked[:, 2, :] ** 2)
+    x = torch.tensor(x).half()
+    y = torch.tensor(y).half()
 
     if save_raw == 1:
 
         for i in range(len(U)):
-            local_U = U[i].view(3, -1)[:2]
-            local_p = p[i].view(1, -1)
+            local_U = U[i].view(3, -1).half()[:2]
+            local_p = p[i].view(1, -1).half()
             torch.save(torch.cat([local_U, local_p], dim=0), solution_dir + ('{:0>8}'.format(str(i))) + "_mesh.th")
         torch.save(x, solution_dir + "x.th")
         torch.save(y, solution_dir + "y.th")
@@ -257,35 +246,24 @@ def process_case(args, current_case_number, work_dir, target_dir, save_raw=1):
         shutil.rmtree(work_dir)
     except Exception as e:
         print(f"Failed to remove {work_dir}: {e}")
-    pool_obj = multiprocessing.Pool()
-    img_list = pool_obj.map(scatter_plot, [(i, x, y, v, triangles, mesh_points) for i in range(U_stacked.shape[0])])
-
-    pool_obj.close()
-
-    for i in range(len(img_list)):
-        img_list[i]._min_frame = 0
-        img_list[i].n_frames = 1
-        img_list[i]._PngImageFile__frame = 0
-
-    img_list[0].save(solution_dir + 'U.gif', format='GIF', append_images=img_list[1:], save_all=True, duration=50,
-                     loop=0)
-
     print("target directory: ", solution_dir)
 
     time.sleep(5)
 
 
-def worker(q, args, case_counter, lock, work_dir, target_dir):
+def worker(q, args, work_dir, dest_dir):
     while True:
-        task_item = q.get()
-        if task_item is None:
+        case_number = q.get()
+        if case_number is None:
+            q.task_done()
             break
-        with lock:
-            current_case_number = case_counter.value
-            case_counter.value += 1
-        print(f'Worker at {work_dir} processing case {current_case_number}')
-        process_case(args, current_case_number, work_dir, target_dir)
-        q.task_done()
+        try:
+            print(f'Worker at {work_dir} processing case {case_number}')
+            process_case(args, case_number, work_dir, dest_dir)
+        except Exception as e:
+            print(f"Case number {case_number} failed: {e}")
+        finally:
+            q.task_done()
 
 
 def remove_all_folders(root_dir):
@@ -299,50 +277,48 @@ def remove_all_folders(root_dir):
                 print(f"Failed to remove {item_path}: {e}")
 
 
-if __name__ == '__main__':
+def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('n_objects', type=int,
                         help='maximum number of circles/partial circles the case should have (min is 1)')
     parser.add_argument('n_cases', type=int, help='number of cases to be run')
-    parser.add_argument('n_workers', type=int, default=multiprocessing.cpu_count(),
+    parser.add_argument('n_workers', type=int,
                         help='Number of worker processes to run in parallel (default: number of CPU cores)')
     parser.add_argument('empty_case', type=str, help='the empty OpenFOAM case directory')
     parser.add_argument('target_dir', type=str, help='target directory for the OpenFOAM Cases')
     parser.add_argument('working_dir', type=str, help='working directory for OpenFOAM Simulation')
     args = parser.parse_args()
-
     assert args.n_objects > 0, "At least one object must be specified"
     assert args.n_workers > 0, "At least one worker must be specified"
+    return args
 
-    target_dir = os.path.join(args.target_dir, '')
 
-    q = multiprocessing.JoinableQueue()
-    manager = multiprocessing.Manager()
-    case_counter = manager.Value('i', get_current_case(target_dir))
-    lock = manager.Lock()
-
-    delta_t = [0.05, 0.025, 0.01, 0.005, 0.0025, 0.001, 0.0005, 0.00025, 0.0001, 0.00005, 0.000025, 0.00001]
-    delta_t_index = 0
-
+if __name__ == '__main__':
+    arguments = parse_args()
+    target_dir = os.path.join(arguments.target_dir, '')
+    queue = multiprocessing.JoinableQueue()
     try:
         processes = []
-        for i in range(args.n_workers):
-            working_dir = os.path.join(args.working_dir, f"worker_{i}", '')
-            os.mkdir(working_dir)
-            p = multiprocessing.Process(target=worker, args=(q, args, case_counter, lock, working_dir, target_dir))
+        for i in range(arguments.n_workers):
+            working_dir = os.path.join(arguments.working_dir, f"worker_{i}", '')
+            os.makedirs(working_dir, exist_ok=True)
+            p = multiprocessing.Process(target=worker, args=(queue, arguments, working_dir, target_dir))
             p.start()
             processes.append(p)
 
-        for task_item in range(args.n_cases):
-            q.put(task_item)
+        start_case = get_cases_count(target_dir)
+        for task_item in range(start_case, start_case + arguments.n_cases):
+            queue.put(task_item)
 
-        q.join()
+        queue.join()
 
-        for _ in range(args.n_workers):
-            q.put(None)
+        for _ in range(arguments.n_workers):
+            queue.put(None)
 
         for p in processes:
             p.join()
+    except Exception as e:
+        print(f"Main process encountered error: {e}")
     finally:
-        remove_all_folders(args.working_dir)
+        remove_all_folders(arguments.working_dir)
         print("All simulation tasks completed.")
